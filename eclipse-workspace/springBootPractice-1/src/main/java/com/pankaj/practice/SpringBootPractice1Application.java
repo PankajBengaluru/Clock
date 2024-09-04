36ecc21d26d6fcb6b5b29bec3ff76c1fd8752113
PankajBengaluru <pankaj.xworkz@gmail.com>
2024-09-05 03:08:13 +0530

    Hello Spring!

---
+++ eclipse-workspace/springBootPractice-1/src/main/java/com/pankaj/practice/SpringBootPractice1Application.java
@@ -0,0 +1,23 @@
+package com.pankaj.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootPractice1Application {
+	@GetMapping("/msg")
+	public String say() {
+		return "Hello, Spring!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractice1Application.class, args);
+		
+}
+}
+
+
+//RestC: 
